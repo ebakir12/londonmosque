014f1986e1dcb860b8ee14c5b4ebc17830883854
Eihab Bakir <eihab.bakir@connectedgateway.com>
2020-04-28 11:17:32 -0500

    sale_subscription_template

---
+++ sale_subscription_template/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Sale Subscription Template",
+
+    'summary': """
+        Sale Subscription Template""",
+
+    'description': """
+        Sale Subscription Template
+    """,
+
+    'author': "Mahmoud Naguib",
+
+    # for the full list
+    'category': 'Sales',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['sale_subscription'],
+
+    # always loaded
+    'data': [
+
+    ],
+
+}
