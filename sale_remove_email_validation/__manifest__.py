cd6c89a7e54efa112b9a228ac70211430b1e1fa1
Eihab Bakir <eihab.bakir@connectedgateway.com>
2020-04-28 09:20:47 -0500

    remove email validation

---
+++ sale_remove_email_validation/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Sale Remove Email Validation",
+
+    'summary': """
+        Sale Remove Email Validation""",
+
+    'description': """
+        Sale Remove Email Validation
+    """,
+
+    'author': "Mahmoud Naguib",
+
+    # for the full list
+    'category': 'Sales',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['website_sale'],
+
+    # always loaded
+    'data': [
+
+    ],
+
+}
