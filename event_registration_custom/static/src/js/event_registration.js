0423c81b3626fb7b282a18010ecdd079466a971d
Eihab Bakir <eihab.bakir@connectedgateway.com>
2020-06-23 21:17:07 -0400

    null value

--- event_registration_custom/static/src/js/event_registration.js
+++ event_registration_custom/static/src/js/event_registration.js
@@ -61,6 +61,7 @@ var _t = core._t;
 	
 
         if(repeated_emails.length && emails[0] != null){
+		alert ('-' + emails[0].value + '-' );
             valid = false;
             var repeated_str = repeated_emails.join(' - ');
             alert('You are already using the email ' + repeated_str + ' for this event registration .. Maximum 1 registration per person is allowed.');
