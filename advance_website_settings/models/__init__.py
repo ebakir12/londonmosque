12785a2ef4975e59635a032353224ee858f6083e
Eihab Bakir <eihab.bakir@connectedgateway.com>
2021-04-06 01:01:46 -0400

    remove modules

--- advance_website_settings/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-#################################################################################
-#
-# Copyright (c) 2018-Present Webkul Software Pvt. Ltd. (<https://webkul.com/>:wink:
-# See LICENSE file for full copyright and licensing details.
-#################################################################################
-from . import advance_website_settings
-from . import inherited_sale_order
-from . import inherit_website
